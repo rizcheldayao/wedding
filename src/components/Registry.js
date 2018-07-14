f3e1e60e7c26ce790be6e58f5894abe332d08b58
Rizchel Dayao <rizchel.a.dayao@gmail.com>
2018-07-14 17:23:54 -0600

    Add registry details

--- src/components/Registry.js
+++ src/components/Registry.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-// import Amazon from '../images/amazon-logo.svg';
-// import Target from '../images/target.svg';
 import Data from '../data/data.json';
 
 class Registry extends Component {
@@ -8,20 +6,13 @@ class Registry extends Component {
     return (
       <div className='registry'>
         <h3>{Data.registryTitle}</h3>
-        <p>Gift registry details coming soon!</p>
-        {/* <p>{Data.registrySubtitle}</p>
-        <p>{Data.thankYou}</p> */}
-        {/* <div className='registry-stores'>
-          <div className='registry-square'>
-            <img className='registry-amazon' src={Amazon} alt='Amazon logo' />
-          </div>
-          <div className='registry-square'>
-            <img className='registry-target' src={Target} alt='Target logo' />
-          </div>
-          <div className='registry-square'>
-            <img className='registry-amazon' src={Amazon} alt='Amazon logo' />
-          </div>
-        </div> */}
+        <p>{Data.registrySubtitle}</p>
+        <p>{Data.thankYou}</p>
+        <div className='registry-stores'>
+          <a href="https://www.target.com/gift-registry/gift/dayao-wood-wedding" target="_blank">Target</a>
+          <a href="https://www.amazon.com/wedding/share/wood-wedding" target="_blank">Amazon</a>
+          <a href="https://www.crateandbarrel.com/gift-registry/rizchel-dayao-and-oliver-wood/r5871264" target="_blank">Crate & Barrel</a>
+        </div>
       </div>
     );
   }
