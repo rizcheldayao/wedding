7a2249b50be0acb06c1bb2a5821b6697a7c3e985
Rizchel Dayao <rizchel.a.dayao@gmail.com>
2018-08-02 20:52:12 -0600

    Add home fund

--- src/components/Registry.js
+++ src/components/Registry.js
@@ -9,9 +9,7 @@ class Registry extends Component {
         <p>{Data.registrySubtitle}</p>
         <p>{Data.thankYou}</p>
         <div className='registry-stores'>
-          <a href="https://www.target.com/gift-registry/gift/dayao-wood-wedding" target="_blank">Target</a>
-          <a href="https://www.amazon.com/wedding/share/wood-wedding" target="_blank">Amazon</a>
-          <a href="https://www.crateandbarrel.com/gift-registry/rizchel-dayao-and-oliver-wood/r5871264" target="_blank">Crate & Barrel</a>
+          <button><a href="https://registry.theknot.com/rizchel-dayao-oliver-wood-april-2019-ca/23670959" target="_blank">View home fund and wedding registry</a></button>
         </div>
       </div>
     );
